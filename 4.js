45bcc02dec2edcd2097bab6e2b657a2fc7804517
Divya-KN123 <92304629+Divya-KN123@users.noreply.github.com>
2021-10-14 11:20:44 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,23 @@
+var option=window.prompt("Please enter the options from 1 to 3:");
+
+switch(option)
+{
+    case 1:
+        console.log("From the question 1 to print pyramid of stars which was done");
+        break;
+    case 2:
+        console.log("From the question 2 to the generation of random marks using the math.random() function was done");
+        break;
+    case 3:
+        console.log("From the question 3 to print the sum and max values using given requirements");
+        break;
+    default:
+        console.log("Please enter any one the values from 1 to 3 only!");
+        break;
+}
+
+
+
+
+
+
